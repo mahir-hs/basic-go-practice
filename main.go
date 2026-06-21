5628ef3a8aae8fadf755b941d4c973ad815cdd6d
hasan.sifat <hasan.sifat@technonext.com>
2026-06-21 11:44:26 +0600

    string formatter

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+
+	//strings
+	var nameOne string = "i am lost,"
+	var nameTwo = "so lost..."
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "oh no!!"
+	nameThree = "i am lost"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	age := 50
+	name := "titus"
+
+	fmt.Println("my name is ", name, " and my age is ", age)
+	fmt.Println("my name is _, my age is _", name, age)
+	fmt.Printf("my name is %v and my age is %v \n", name, age)
+	fmt.Printf("my name is %q and my age is %q \n", name, age)
+	fmt.Printf("age is type of %T \n", age)
+	fmt.Printf("you scorred %0.2f marks \n", 500.159)
+
+	str := fmt.Sprintf("my name is %v and my age is %v \n", name, age)
+
+	fmt.Println(str)
 }
