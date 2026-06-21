cdc7cf970a45a72358713175c039a42b39cbb984
hasan.sifat <hasan.sifat@technonext.com>
2026-06-21 12:22:48 +0600

    loop and bool

--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
+import "fmt"
 
 func main() {
+	x := 0
+	for x < 5 {
+		fmt.Println(x)
+		x++
+	}
 
-	greeting := "hello world"
-	fmt.Println(strings.Contains(greeting, "hello"))
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hola!"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "ll"))
-	fmt.Println(strings.Split(greeting, " "))
+	names := []string{"aa", "bb", "cc"}
 
-	fmt.Println("Original string = ", greeting)
+	for index, value := range names {
+		fmt.Println(index, value)
+	}
 
-	ages := []int{4, 10, 5, 9, 55, 78, 6, 7, 7}
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 7)
-	fmt.Println(index)
+	if x <= 0 {
+		fmt.Println("value is not changed!")
+	} else if x > 0 {
+		fmt.Println("value got changed")
+	}
 }
